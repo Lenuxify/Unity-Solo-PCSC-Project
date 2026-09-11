c26ff542f6d607b791487f317d645c1e65ccffff
Levi <levisuom@gmail.com>
2026-09-11 10:24:02 -0700

    Player, Floor, Movement Code

--- Lim-Suom_SoloProject/Assets/Scripts/PlayerController.cs
+++ Lim-Suom_SoloProject/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float speed = 5.0f;
+    public float jumpHeight = 10f;
+
+    PlayerInput playerInput;
+    Rigidbody rb;
+
+    Vector2 moveInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Fetch Components into an variable
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+        // Set up new move vector
+        moveInput = new Vector2();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 tempMovement = rb.linearVelocity;
+
+        rb.linearVelocity = (moveInput.x * speed) + (moveInput.y * speed) + (moveInput.z * speed);
+    }
+
+    // Read context of input
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
     }
 }
